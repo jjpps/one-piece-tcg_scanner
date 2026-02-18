82d6921ccf6ff2393e113fb20e54225817a72926
Joao Pedro <joao.policastro_ext@pernambucanas.com.br>
2026-02-18 17:44:20 -0300

    WIP: input card Images

--- frontend/scanner-frontend/src/app/app.routes.ts
+++ frontend/scanner-frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ScanCardComponent } from './scan-card-component/scan-card-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'ScanCards',
+        component: ScanCardComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
---
+++ frontend/scanner-frontend/src/app/scan-card-component/scan-card-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scan-card-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './scan-card-component.html',
+  styleUrls: ['./scan-card-component.css'],
+})
+export class ScanCardComponent {
+
+}
